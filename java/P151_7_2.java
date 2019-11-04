8321b23a22bd8e0865d88a16cde4c08ccb2b78bc
Sande <36633585+sande2018@users.noreply.github.com>
2019-11-04 07:21:49 +0000

    Create P151_7_2.java

---
+++ java/P151_7_2.java
@@ -0,0 +1,32 @@
+-------------demo.java--------------------
+package P151_7_2;
+
+import java.util.Scanner;
+import P151_7_2.triangle;;
+public class demo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		triangle w=new triangle();
+		try {
+			w.triangle(a, b, c);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
+-------------triangle.java--------------------
+package P151_7_2;
+
+public class triangle extends demo{
+	public void triangle(int a, int b, int c) throws Exception {
+		if ( a+b>c && a+c>b && b+c>a) {
+			System.out.println("可以组成三角形");
+		} else {
+			throw new Exception("不能组成三角形");
+		}
+	}
+}
+
