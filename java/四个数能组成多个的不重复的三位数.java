58c60e40794e249bdaac880e59803fdf588e815c
Sande <36633585+sande2018@users.noreply.github.com>
2019-11-18 14:34:50 +0800

    Create 四个数能组成多个的不重复的三位数.java

---
+++ java/四个数能组成多个的不重复的三位数.java
@@ -0,0 +1,19 @@
+package Demo_01;
+
+public class Demo_01 {
+	public static void main(String[] args) {
+		/*有1，2，3，4四个数，能组成多少个不相同且无重复数字的三位数？*/
+		int i,j,k,count=0;
+		for (i = 1; i < 5; i++) {
+			for (j = 1; j < 5; j++) {
+				for (k = 1; k < 5; k++) {
+					if (i!=j && i!=k && j!=k) {
+						count++;
+						System.out.printf("第%d组 %d %d %d\n",count,i,j,k);
+					}
+				}
+			}
+		}
+	}
+
+}
