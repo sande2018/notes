b7b111d890439c69bee9cfb1c2b288962526dfdc
Sande <36633585+sande2018@users.noreply.github.com>
2019-11-18 15:00:29 +0800

    Update 四个数能组成多个的不重复的三位数.java

--- java/四个数能组成多个的不重复的三位数.java
+++ java/四个数能组成多个的不重复的三位数.java
@@ -18,3 +18,29 @@ public class Demo_01 {
 	}
 
 }
+
+--------------------------------------------------------------
+package Demo_03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Demo_03 {
+	/*输入xyz三个整数，请把这三个数从小到大输出。*/
+	public static void main(String[] args) {
+		int num[]=new int[3];
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		System.out.print("请输入x值：");
+		num[0] = sc.nextInt();
+		System.out.print("请输入y值：");
+		num[1] = sc.nextInt();
+		System.out.print("请输入z值：");
+		num[2] = sc.nextInt();
+		Arrays.sort(num);
+		System.out.print("从小到大排列：");
+		for (int i = 0; i < num.length; i++) {
+			System.out.printf("%d ",num[i]);
+		}
+	}
+}
